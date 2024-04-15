f27b3838a432a01a2300e0798e89e90360473bf9
Juliiaks <150396643+Juliiaks@users.noreply.github.com>
2024-04-15 12:50:36 +0000

    Deploying to gh-pages from @ Juliiaks/goit-js-hw-12@9890665d41317b8caed0396f03a88a00308f20fd 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,8 +1,8 @@
-import{S as f,i}from"./assets/vendor-8c59ed88.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))t(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const n of s.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&t(n)}).observe(document,{childList:!0,subtree:!0});function o(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function t(e){if(e.ep)return;e.ep=!0;const s=o(e);fetch(e.href,s)}})();function d(a){const r="43264950-7d4cd6be2017577f15438949e",o=new URLSearchParams({key:r,q:a,image_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`https://pixabay.com/api/?${o}`).then(t=>{if(!t.ok)throw new Error(t.status);return t.json()})}const m=new f(".gallery a",{captionsData:"alt",captionsPosition:"bottom",captionDelay:250});function g(a){document.querySelector(".gallery").innerHTML="";const r=a.map(({webformatURL:o,largeImageURL:t,tags:e,likes:s,views:n,comments:u,downloads:p})=>`<li class="gallery-item">
-  <a class="gallery-link" href="${t}">
+import{a as y,S as b,i as c}from"./assets/vendor-06b1bbdf.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const l of s.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&r(l)}).observe(document,{childList:!0,subtree:!0});function a(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function r(e){if(e.ep)return;e.ep=!0;const s=a(e);fetch(e.href,s)}})();async function d(o,t){const a="43264950-7d4cd6be2017577f15438949e";return(await y.get("https://pixabay.com/api/",{params:{key:a,q:o,image_type:"photo",orientation:"horizontal",safesearch:!0,page:t,per_page:15}})).data}const v=new b(".gallery a",{captionsData:"alt",captionsPosition:"bottom",captionDelay:250});function p(o){const t=o.map(({webformatURL:a,largeImageURL:r,tags:e,likes:s,views:l,comments:m,downloads:h})=>`<li class="gallery-item">
+  <a class="gallery-link" href="${r}">
     <img
       class="gallery-image"
-      src="${o}"
+      src="${a}"
       alt="${e}"
     />
   </a>
@@ -13,16 +13,16 @@ import{S as f,i}from"./assets/vendor-8c59ed88.js";(function(){const r=document.c
   </p>
   <p class="field">
   <span class="text">Views</span>
-  <span class="value">${n}
+  <span class="value">${l}
   </p>
   <p class="field">
   <span class="text">Comments</span>
-  <span class="value">${u}</span>
+  <span class="value">${m}</span>
   </p >
   <p class="field">
   <span class="text">Downloads</span>
-  <span class="value">${p}</span>
+  <span class="value">${h}</span>
   </p>
   </div>
-</li>`).join(" ");document.querySelector(".gallery").insertAdjacentHTML("beforeend",r),m.refresh()}const c=document.querySelector(".form");document.querySelector("button");const l=document.querySelector(".loader");c.addEventListener("submit",y);function y(a){a.preventDefault();const o=document.querySelector("#pictures").value.trim();if(document.querySelector(".gallery").innerHTML="",o===""){console.log("empty"),i.show({message:"Sorry, there are no images matching your search query. Please try again!",messageColor:"#fff",position:"topRight",backgroundColor:"#ef4040",progressBar:!1,close:!1,timeout:5e3});return}l.classList.remove("hide"),d(o).then(t=>{t.hits.length===0&&i.show({message:"Sorry, there are no images matching your search query. Please try again!",messageColor:"#fff",position:"topRight",backgroundColor:"#ef4040",progressBar:!1,close:!1,timeout:5e3}),console.log(t),g(t.hits)}).catch(t=>{console.log("catch",t)}).finally(()=>{c.reset(),l.classList.add("hide")})}
+</li>`).join(" ");document.querySelector(".gallery").insertAdjacentHTML("beforeend",t),v.refresh()}const f=document.querySelector(".form");document.querySelector("button");const u=document.querySelector(".loader"),g=document.querySelector(".load-button");let n=1,i=null;f.addEventListener("submit",L);g.addEventListener("click",S);async function L(o){if(o.preventDefault(),document.querySelector("#pictures"),document.querySelector(".gallery").innerHTML="",i===""){console.log("empty"),c.show({message:"Sorry, there are no images matching your search query. Please try again!",messageColor:"#fff",position:"topRight",backgroundColor:"#ef4040",progressBar:!1,close:!1,timeout:5e3});return}u.classList.remove("hide"),i=o.currentTarget.elements.pictures.value.trim();try{const t=await d(i,n);t.hits.length===0&&c.show({message:"Sorry, there are no images matching your search query. Please try again!",messageColor:"#fff",position:"topRight",backgroundColor:"#ef4040",progressBar:!1,close:!1,timeout:5e3}),console.log(t),p(t.hits),t.hits.length>15&&g.remove("hide")}catch(t){console.log("catch",t)}finally{f.reset(),u.classList.add("hide")}}async function S(){n+=1;try{const o=await d(i,n);p(o.hits);const{height:t}=document.querySelector(".gallery").firstElementChild.getBoundingClientRect();if(window.scrollBy({top:t*2,behavior:"smooth"}),Math.ceil(o.total/15)===n)return c.show({message:"We're sorry, but you've reached the end of search results.",messageColor:"#fff",position:"topRight",backgroundColor:"light blue",progressBar:!1,close:!1,timeout:5e3})}catch(o){console.log(o.message)}finally{u.classList.add("hide")}}
 //# sourceMappingURL=commonHelpers.js.map
